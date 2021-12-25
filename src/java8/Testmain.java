4e89371ecc7348962468076fe9309f8514940a73
kolasatya <satyakola777@gmail.com>
2021-12-25 21:17:35 +0530

    removed unused imports

--- src/java8/Testmain.java
+++ src/java8/Testmain.java
@@ -1,10 +1,8 @@
 package java8;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
-import java.util.stream.Collectors;
 
 public class Testmain   {
 
